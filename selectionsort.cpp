15447bcc1e4fe76bc9b38df3e258eb4decde161f
Dom-Software-Modeling-MusicProduction <cristellodominic@gmail.com>
2026-01-06 15:51:42 -0500

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -8,7 +8,7 @@ void selectionsort(int arr[], int size){
     int minindex = 0;
     while(minindex < size){
         int smallest = minindex;
-        for (int i = minindex; i < size; i++){
+        for (int i = minindex; i < size; i++){ // scan the portion of the unsorted list to find the smallest value
             if (arr[i] < arr[smallest]){
                smallest = i;
             }
@@ -23,8 +23,7 @@ void selectionsort(int arr[], int size){
 
 int main(){
     srand(time(NULL));
-    int size = 1000;
-    int target = 45;
+    int size = 100;
     int arr[size];
     arr[0] = 0;
     for (int i = 1; i < size; i++){
@@ -33,6 +32,6 @@ int main(){
     }
     selectionsort(arr, size);
     for (int i = 0; i < size; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << endl; //print out each element of the list sorted in order, each on a new line
     }
 }
