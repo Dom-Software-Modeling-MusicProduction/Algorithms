fedc9ded8f33dceec4def1c5612f7af080ad9c13
Dom-Software-Modeling-MusicProduction <cristellodominic@gmail.com>
2026-01-06 15:38:32 -0500

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -11,7 +11,7 @@ void bubblesort(int arr[], int size){
         for (int i = 0; i < size-1; i++){
             if (arr[i] > arr[i+1]){ //if the next element is smaller than the current, swap
                 int temp = arr[i]; // keep arr[i] stored as a temp variable since in the next line, it's being rewritten
-                arr[i] = arr[i+1];
+                arr[i] = arr[i+1]; //set the next item in the array as the current item in the array
                 arr[i+1] = temp;
                 swapped = true;
             }
@@ -21,16 +21,16 @@ void bubblesort(int arr[], int size){
 
 int main(){
     srand(time(NULL));
-    int size = 1000;
+    int size = 100;
     int target = 45;
     int arr[size];
     arr[0] = 0;
     for (int i = 1; i < size; i++){
-        arr[i] = rand()%20000;
+        arr[i] = rand()%20000; //make a random number between 1 and 20,000
         
     }
     bubblesort(arr, size);
     for (int i = 0; i < size; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << endl; //print out all the numbers in the array sorted, each on a new line
     }
 }
